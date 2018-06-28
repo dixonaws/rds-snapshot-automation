5e3dc515daafc668cc9b7782b309fb79d7d44fd3
dixonaws <dixonaws@amazon.com>
2018-06-27 22:29:36 -0400

    Added ec2_metadata library to grab instance info

--- create-db-snapshot.py
+++ create-db-snapshot.py
@@ -1,12 +1,19 @@
 import boto3
 import time
+from ec2_metadata import ec2_metadata
 
 
 def main():
     print("Logging to CloudWatchLogs...")
 
-	# cloudwatchlogs expects unix time * 1000, otherwise throws TooOldLogEventsException
-    intTime = int(time.time())*1000
+    # cloudwatchlogs expects unix time * 1000, otherwise throws TooOldLogEventsException
+    intTime = int(time.time()) * 1000
+
+    try:
+        strInstanceId=ec2_metadata.instance_id
+    except TimeoutError:
+        strInstanceId="testing_non_ec2"
+        pass
 
     CloudWatchLogsClient = boto3.client('logs', region_name='us-east-1')
 
@@ -17,17 +24,19 @@ def main():
 
     # print(response)
 
-    strNextToken=response['logStreams'][0]['uploadSequenceToken']
+    strNextToken = response['logStreams'][0]['uploadSequenceToken']
     print("Token: " + strNextToken)
 
     # log to CloudWatch
+    strMessage='Logged from instanceId ' + strInstanceId
+
     response = CloudWatchLogsClient.put_log_events(
         logGroupName='rds-snapshot-automation-logs',
         logStreamName='rds-instance',
         logEvents=[
             {
                 'timestamp': intTime,
-                'message': 'I logged!'
+                'message': strMessage
             },
         ],
 
