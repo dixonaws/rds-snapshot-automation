1b6a89b87b850a53cbb2d6471f04275b1b1be157
dixonaws <dixonaws@amazon.com>
2018-06-27 22:09:15 -0400

    Cloudwatchlogs client working

--- create-db-snapshot.py
+++ create-db-snapshot.py
@@ -1,37 +1,40 @@
 import boto3
 import time
 
+
 def main():
-	print("Logging to CloudWatchLogs...")
+    print("Logging to CloudWatchLogs...")
 
-	intTime=int(time.time())
+	# cloudwatchlogs expects unix time * 1000, otherwise throws TooOldLogEventsException
+    intTime = int(time.time())*1000
 
-	CloudWatchLogsClient=boto3.client('logs', region_name='us-east-1')
+    CloudWatchLogsClient = boto3.client('logs', region_name='us-east-1')
 
-	# get the next token from CloudWatch so that we can put event in the log stream
-	response=CloudWatchLogsClient.describe_log_streams(
-		logGroupName='rds-snapshot-automation-logs'
-	)
+    # get the next token from CloudWatch so that we can put event in the log stream
+    response = CloudWatchLogsClient.describe_log_streams(
+        logGroupName='rds-snapshot-automation-logs'
+    )
 
-	#print(response)
+    # print(response)
 
-	#strNextToken=response['logStreams'][0]['uploadSequenceToken']
-	#print("Token: " + strNextToken)
+    strNextToken=response['logStreams'][0]['uploadSequenceToken']
+    print("Token: " + strNextToken)
 
-	# log to CloudWatch
-	response=CloudWatchLogsClient.put_log_events(
-		logGroupName='rds-snapshot-automation-logs',
-		logStreamName='rds-instance',
-		logEvents=[
-			{
-				'timestamp': intTime,
-				'message': 'I logged!'
-			},
-		]
+    # log to CloudWatch
+    response = CloudWatchLogsClient.put_log_events(
+        logGroupName='rds-snapshot-automation-logs',
+        logStreamName='rds-instance',
+        logEvents=[
+            {
+                'timestamp': intTime,
+                'message': 'I logged!'
+            },
+        ],
 
-		#sequenceToken=strNextToken
-	)
+        sequenceToken=strNextToken
+    )
 
-	print(response)
+    print(response)
+
 
 main()
