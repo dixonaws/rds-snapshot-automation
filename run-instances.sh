7153f5fb9ba6a9968bc36ab8fffa1fd29911a33d
dixonaws@amazon.com <dixonaws@amazon.com>
2018-06-28 19:54:39 -0400

    Updated June 28

---
+++ run-instances.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+aws ec2 run-instances --image-id ami-b70554c8 --count 1 --instance-type t2.micro --key-name dixonaws --security-group-ids sg-28fe8e63 --subnet-id subnet-e8e4718d --iam-instance-profile Name=EC2Role_AdministratorAccess --user-data file://userdata.txt
+
+# do you need the subnet-ids parameter?
+# aws ec2 run-instances --image-id ami-b70554c8 --count 1 --instance-type t2.micro --key-name dixonaws --security-group-ids ami-b70554c8 --subnet-id subnet-6e7f829e
